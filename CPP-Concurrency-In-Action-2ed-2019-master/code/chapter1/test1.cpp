b3804d2aa7d8e4afa046b924c7ff4cddb9205c67
liyuchong <liyuchong@megvii.com>
2020-09-30 11:34:54 +0800

    init

---
+++ CPP-Concurrency-In-Action-2ed-2019-master/code/chapter1/test1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>  // 1
+void hello()  // 2
+{
+  std::cout << "Hello Concurrent World\n";
+}
+int main()
+{
+  std::thread t(hello);  // 3
+  t.join();  // 4
+}
